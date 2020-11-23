7341ea7ba70a10af19efb8b2dd05e37e90cbeea3
AndranikYeritsyan <56552493+AndranikYeritsyan@users.noreply.github.com>
2020-11-23 20:32:40 +0400

    Add files via upload

---
+++ hmw3.js
@@ -0,0 +1,102 @@
+//ex 1
+// let evenNumbers = [];
+// let oddNumbers = [];
+// function allNubersArr(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] % 2 == 0) {
+//             evenNumbers.push(arr[i]);
+//         } else {
+//             oddNumbers.push(arr[i]);
+//         }
+//     }
+//     let myObj = { oddNumbers, evenNumbers }
+//     return myObj
+// }
+// fn = allNubersArr([22,37,115,208,1554,1, 2, 3, 4, 5, 6, 7, 8, 9]);
+// console.log(fn)
+
+//ex2
+// let newArr = [];
+// function filterRange(arr, a, b) {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] > a && arr[i] < b) {
+//             newArr.push(arr[i]);
+//         }
+//     }
+//     return newArr;
+// }
+// fn = filterRange([1, 2, 5, 10, 12, 15, 18, 25], 5, 26);
+// console.log(fn);
+
+
+//ex3
+// const frameworks = [
+//     { id: 1, title: "react" },
+//     { id: 2, title: "angular" },
+//     { id: 3, title: "vue" }
+// ]
+// function findById(frameworks, num) {
+//     for (i = 0; i < frameworks.length; i++) {
+//         if (frameworks[i].id === num) {
+//             return frameworks[i]
+//         }
+//     }
+//     return "No Data";
+// }
+// console.log(findById(frameworks, 2));
+
+
+
+//ex4
+// arr = ["Godfather", "Whiplash", "Social   Network"];
+// myNewGeneretedArr = [];
+// function generate(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         myNewGeneretedArr.push({});
+//         myNewGeneretedArr[i].title = arr[i];
+//         myNewGeneretedArr[i].idx = i;
+//     }
+//     return myNewGeneretedArr;
+// }
+// console.log(generate(arr));
+
+
+//ex5
+// arr = ["Godfather", "Whiplash", "Social   Network"];
+// function removeByLetter(arr, letter) {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i][0] == letter.toUpperCase() || arr[i][0] == letter.toLowerCase()) {
+//             arr.splice(i, 1);
+//         }
+//     }
+//     return arr;
+// }
+// console.log(removeByLetter(arr, "g"));
+
+
+//ex6
+// function repeatLonger(str1, str2) {
+//     // let letterDifference = Math.abs(str1.length - str2.length);
+//     let finaly = " ";
+//     if (str1.length > str2.length) {
+//         let roundedQuotient = Math.ceil(str1.length / str2.length);
+//         let d = str2.repeat(roundedQuotient);
+//         let arr = d.split('');
+//         let finallyArr = arr.slice(0, str1.length);
+//         for (i = 0; i < finallyArr.length; i++) {
+//             finaly += finallyArr[i];
+//         }
+//     } else {
+//         let roundedQuotient = Math.ceil(str2.length / str1.length);
+//         let d = str1.repeat(roundedQuotient);
+//         let arr = d.split('');
+//         let finallyArr = arr.slice(0, str2.length)
+//         for (i = 0; i < finallyArr.length; i++) {
+//             finaly += finallyArr[i];
+//         }
+
+//     }
+//     return finaly;
+// }
+// fn = repeatLonger("Da", "123456789")
+// console.log(fn)
